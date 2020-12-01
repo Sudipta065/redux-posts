08db6aeb3d03dc7a3f10f37155343cefac0f38e3
Sudipta Adhikary <58800767+Sudipta065@users.noreply.github.com>
2020-12-01 12:52:18 +0600

    A redux blogpost website

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+import _ from "lodash";
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceHolder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
